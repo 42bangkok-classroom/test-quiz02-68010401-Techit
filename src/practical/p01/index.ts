1f01718608e882b778b8e3b857cc1b89554d993c
TEK <68010401@kmitl.ac.th>
2026-01-16 19:30:24 +0700

    4

--- src/practical/p01/index.ts
+++ src/practical/p01/index.ts
@@ -2,6 +2,31 @@ import axios from "axios";
 export async function getPostalAddress() {
     const res = await axios.get("https://jsonplaceholder.typicode.com/users");
   return res.data;
+  type User = {
+    id: number;
+    name: string;
+    phone: string;
+    address: {
+      street: string;
+      suite: string;
+      city: string;
+      zipcode: string;
+      geo: {
+        lat: string;
+        lng: string;
+      };
+    };
 }
+    const users: User[] = res.data;
+    return users.map((user) => ({
+      id: user.id,
+      name: user.name,
+      phone: user.phone,
+      address: `${user.address.street}, ${user.address.suite}, ${user.address.city}, ${user.address.zipcode} (Lat: ${user.address.geo.lat}, Lng: ${user.address.geo.lng})`,
+    }));
+}
+
+
+
 
 
